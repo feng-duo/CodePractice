e0155db9143319af944f33723d2df9b53a4fedf3
FengDuo <feng_duo@foxmail.com>
2021-11-24 00:00:00 +0800

    init

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void method1() {
+        System.out.println("sdfsdfds");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+    }
+
+}
