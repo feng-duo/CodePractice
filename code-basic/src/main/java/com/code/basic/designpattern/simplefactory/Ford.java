7da2d0f561f886e013953ca3173b562ea56d01fa
yufeng.song <syf@sxw.cn>
2022-06-24 11:35:03 +0800

    feat: add factory design pattern code example.

---
+++ code-basic/src/main/java/com/code/basic/designpattern/simplefactory/Ford.java
@@ -0,0 +1,12 @@
+package com.code.basic.designpattern.simplefactory;
+
+/**
+ * @author FengDuo
+ * @date 2022/6/24 11:05
+ */
+public class Ford implements Car{
+    @Override
+    public String description() {
+        return "This is a Ford.";
+    }
+}
