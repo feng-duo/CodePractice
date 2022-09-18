cabcae5756157ee4ec18e172bc7a2d187852929f
FengDuo <feng_duo@foxmail.com>
2022-09-18 23:05:35 +0800

    refactor: submit canal demo.

---
+++ canal/src/main/java/com/code/canal/CanalWebApplication.java
@@ -0,0 +1,17 @@
+package com.code.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author FengDuo
+ * @date 2022/09/18 16:49
+ */
+@SpringBootApplication
+public class CanalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalWebApplication.class, args);
+    }
+
+}
