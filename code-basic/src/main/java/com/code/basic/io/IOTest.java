f2101a2af3f8e4f71256384855c22e1064d7cda3
FengDuo <feng_duo@foxmail.com>
2022-11-30 23:48:13 +0800

    submit ClassLoader related code.

--- code-basic/src/main/java/com/code/basic/io/IOTest.java
+++ code-basic/src/main/java/com/code/basic/io/IOTest.java
@@ -1,12 +1,26 @@
 package com.code.basic.io;
 
+import java.io.*;
+
 /**
  * @author FengDuo
- * @date 2022/11/16 16:39
+ * @date 2022/11/18 18:09
  */
 public class IOTest {
 
     public static void main(String[] args) {
+        InputStream inputStream = IOTest.class.getResourceAsStream("/com/code/basic/io/etc/abc.txt");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        LineNumberReader lineNumberReader = new LineNumberReader(bufferedReader);
+        try {
+            while (lineNumberReader.read() > 0) {
+                System.out.println(lineNumberReader.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
 }
