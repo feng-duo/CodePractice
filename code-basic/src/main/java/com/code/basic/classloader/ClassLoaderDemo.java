615c890276de9c28d10e5b058fa29addea3f4f93
FengDuo <feng_duo@foxmail.com>
2022-12-16 17:26:55 +0800

    submit classloader demo.

---
+++ code-basic/src/main/java/com/code/basic/classloader/ClassLoaderDemo.java
@@ -0,0 +1,12 @@
+package com.code.basic.classloader;
+
+/**
+ * @author FengDuo
+ * @date 2022/12/16 17:19
+ */
+public class ClassLoaderDemo {
+
+    static class MyClassLoader extends ClassLoader {
+
+    }
+}
