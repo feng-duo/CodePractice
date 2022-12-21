06422cab17eb9af954fe30df1a82edcc69760dda
FengDuo <feng_duo@foxmail.com>
2022-12-21 17:15:51 +0800

    feat(code-basic): add factory pattern related code
    add factory method pattern and abstract factory pattern related code.

--- code-basic/src/main/java/com/code/basic/designpattern/factory/FactoryTest.java
+++ code-basic/src/main/java/com/code/basic/designpattern/factory/FactoryTest.java
@@ -1,5 +1,7 @@
 package com.code.basic.designpattern.factory;
 
+import org.junit.Test;
+
 /**
  * @author FengDuo
  * @date 2022/6/24 11:10
@@ -7,7 +9,19 @@ package com.code.basic.designpattern.factory;
 public class FactoryTest {
 
     public static void main(String[] args) {
+        // simple factory test
         System.out.println(CarFactory.getCar(CarType.FERRARI).description());
         System.out.println(CarFactory.getCar(CarType.FORD).description());
+    }
+
+
+    @Test
+    public void PizzaTest(){
+        // factory method test
+        PizzaStore newYorkPizzaStore = new NewYorkPizzaStore();
+        PizzaStore chicagoPizzaStore = new ChicagoPizzaStore();
+        newYorkPizzaStore.orderPizza(PizzaType.NEWYORK_CHEESE.name());
+        System.out.println("----------------");
+        chicagoPizzaStore.orderPizza(PizzaType.CHICAGO_CHEESE.name());
     }
 }
