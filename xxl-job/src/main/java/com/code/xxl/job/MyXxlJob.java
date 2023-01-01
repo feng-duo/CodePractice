1f57f137f90afdc346f5837000cd03f0fb61c170
FengDuo <feng_duo@foxmail.com>
2023-01-02 01:00:00 +0800

    refactor(xxl-job): update xxl-job demo.

--- xxl-job/src/main/java/com/code/xxl/job/MyXxlJob.java
+++ xxl-job/src/main/java/com/code/xxl/job/MyXxlJob.java
@@ -12,8 +12,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyXxlJob {
 
+    /**
+     * 服务端通过docker部署在华为云上，内网执行器无法执行任务，需要内网穿透
+     */
     @XxlJob("MyFirstXxlJob")
     public void MyFirstXxlJob(){
+        System.out.println("------------------");
         log.info("Hello xxl-job.");
     }
 }
