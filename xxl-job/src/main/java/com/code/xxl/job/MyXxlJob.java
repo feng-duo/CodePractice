be2cbd808c466f12d96dd74faef3ab06eeb2954f
FengDuo <feng_duo@foxmail.com>
2023-01-03 17:14:44 +0800

    build(code-practice): update maven build related pom.

--- xxl-job/src/main/java/com/code/xxl/job/MyXxlJob.java
+++ xxl-job/src/main/java/com/code/xxl/job/MyXxlJob.java
@@ -4,6 +4,8 @@ import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 /**
  * @author FengDuo
  * @date 2022/12/29 23:46
@@ -16,8 +18,7 @@ public class MyXxlJob {
      * 服务端通过docker部署在华为云上，内网执行器无法执行任务，需要内网穿透
      */
     @XxlJob("MyFirstXxlJob")
-    public void MyFirstXxlJob(){
-        System.out.println("------------------");
-        log.info("Hello xxl-job.");
+    public void MyFirstXxlJob() {
+        log.info("Hello xxl-job, {}.", LocalDateTime.now());
     }
 }
