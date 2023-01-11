93171d84ceb910bd5dca916f7139ab683beaaa8d
FengDuo <feng_duo@foxmail.com>
2023-01-11 17:17:09 +0800

    feat(code-basic): add logback demo.

--- code-basic/src/main/java/com/code/basic/designpattern/facade/FacadeTest.java
+++ code-basic/src/main/java/com/code/basic/designpattern/facade/FacadeTest.java
@@ -8,7 +8,6 @@ package com.code.basic.designpattern.facade;
  */
 public class FacadeTest {
 
-
     public static void main(String[] args) {
         Amplifier amp = new Amplifier("Top-O-Line Amplifier");
         Tuner tuner = new Tuner("Top-O-Line AM/FM Tuner", amp);
@@ -20,7 +19,7 @@ public class FacadeTest {
         PopcornPopper popper = new PopcornPopper("Popcorn Popper");
 
         HomeTheaterFacade homeTheater = new HomeTheaterFacade(amp, tuner, dvd, cd,
-                        projector, screen, lights, popper);
+                projector, screen, lights, popper);
 
         homeTheater.watchMovie("Raiders of the Lost Ark");
         homeTheater.endMovie();
---
+++ code-basic/src/main/java/com/code/basic/log/LogDemo.java
@@ -0,0 +1,17 @@
+package com.code.basic.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author FengDuo
+ * @date 2023/1/11 10:50
+ */
+@Slf4j
+public class LogDemo {
+
+    @Test
+    public void logTest() {
+        // https://www.cnblogs.com/simpleito/p/15133654.html
+    }
+}
