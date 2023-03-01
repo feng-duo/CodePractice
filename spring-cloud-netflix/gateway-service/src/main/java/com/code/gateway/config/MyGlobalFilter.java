374a0f86181878ca5a4fa2700a2f17f72ddf569d
FengDuo <feng_duo@foxmail.com>
2023-03-01 17:28:55 +0800

    refactor(spring-cloud-netflix): update gateway module.

--- spring-cloud-netflix/gateway-service/src/main/java/com/code/gateway/config/MyGlobalFilter.java
+++ spring-cloud-netflix/gateway-service/src/main/java/com/code/gateway/config/MyGlobalFilter.java
@@ -5,8 +5,6 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -22,16 +20,43 @@ public class MyGlobalFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("filter start.");
-        if (exchange.getRequest().getMethod().matches(HttpMethod.GET.name())) {
-            exchange.getResponse().setStatusCode(HttpStatus.BAD_REQUEST);
-            return exchange.getResponse().setComplete();
-        }
-        log.info("filter end.");
-        return chain.filter(exchange);
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> log.info("filter end.")));
     }
 
     @Override
     public int getOrder() {
         return 0;
     }
+/*    @Bean
+    @Order(-1)
+    public GlobalFilter a() {
+        return (exchange, chain) -> {
+            log.info("first pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("first post filter");
+            }));
+        };
+    }
+
+    @Bean
+    @Order(0)
+    public GlobalFilter b() {
+        return (exchange, chain) -> {
+            log.info("second pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("second post filter");
+            }));
+        };
+    }
+
+    @Bean
+    @Order(1)
+    public GlobalFilter c() {
+        return (exchange, chain) -> {
+            log.info("third pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("third post filter");
+            }));
+        };
+    }*/
 }
