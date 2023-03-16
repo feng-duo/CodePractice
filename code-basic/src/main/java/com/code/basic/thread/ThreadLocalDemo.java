86a08600c2dece2c9a8dfe95f084f1b02c4a2a06
FengDuo <feng_duo@foxmail.com>
2023-03-16 22:59:16 +0800

    build(code-basic): init ThreadLocal demo.

---
+++ code-basic/src/main/java/com/code/basic/thread/ThreadLocalDemo.java
@@ -0,0 +1,12 @@
+package com.code.basic.thread;
+
+/**
+ * @author FengDuo
+ * @date 2023/03/16 22:10
+ */
+public class ThreadLocalDemo {
+
+    public static void main(String[] args) {
+//        http://www.coketea.com/2017/12/18/li-jie-threadlocal-yi-ji-qi-zai-dai-ma-diao-shi-zhong-peng-dao-de-wen-ti-zong-jie/
+    }
+}
