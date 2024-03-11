0a0ed2ef9ab58d03d1c2e083040833ac5e8cb822
FengDuo <feng_duo@foxmail.com>
2024-03-11 16:40:42 +0800

    build(code-basic): update linked list related algorithm.

--- code-basic/src/main/java/com/code/basic/algorithm/node/AddTwoNodes.java
+++ code-basic/src/main/java/com/code/basic/algorithm/node/AddTwoNodes.java
@@ -2,13 +2,24 @@ package com.code.basic.algorithm.node;
 
 
 /**
+ * 两数相加
+ * <p>
  * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
  * 请你将两个数相加，并以相同形式返回一个表示和的链表。
  * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
  * <p>
+ * 示例 1：
  * 输入：l1 = [2,4,3], l2 = [5,6,4]
  * 输出：[7,0,8]
  * 解释：342 + 465 = 807.
+ * <p>
+ * 示例 2：
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ * <p>
+ * 示例 3：
+ * 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * 输出：[8,9,9,9,0,0,0,1]
  *
  * @author FengDuo
  * @date 2023/11/16 14:58
@@ -63,5 +74,28 @@ public class AddTwoNodes {
         }
         return result.next;
     }
+
+/*
+    // 递归做法
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return addNumber(l1, l2, 0);
+    }
+
+    public ListNode addNumber(ListNode l1, ListNode l2, Integer extraNum) {
+        if (l2 != null || l1 != null || extraNum > 0) {
+            ListNode node = new ListNode(0);
+            int num = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+            node.val = extraNum + num;
+            if (node.val > 9) {
+                extraNum = 1;
+                node.val -= 10;
+            } else {
+                extraNum = 0;
+            }
+            node.next = addNumber(l1 == null ? null : l1.next, l2 == null ? null : l2.next, extraNum);
+            return node;
+        }
+        return null;
+    }*/
 
 }
